e554ef7b98dfc27c11db418c1d04ff37b7c8e752
kinyunye <kamaugerald2@gmail.com>
2018-07-02 02:42:52 +0300

    Firebase Data Persistence

---
+++ app/src/main/java/com/example/finch/myjournal/MyJournal.java
@@ -0,0 +1,16 @@
+package com.example.finch.myjournal;
+
+import android.app.Application;
+
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MyJournal extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+    }
+}
